b315cb46963f9c6f8d219dfa272b66fcd5692a80
unknown <camachoedgar23@gmail.com>
2019-09-01 14:12:09 -0600

    GeneratorExcepciones

--- Generator.ts
+++ Generator.ts
@@ -1,12 +1,15 @@
-function* logGenerator() {
-    console.log(yield);
-    console.log(yield);
-    console.log(yield);
+function* generator() {
+    try {
+        yield 'foo';
+        throw Error("Test");
+    }
+    catch(err) {
+    console.log(err.message); // bar!
+    }
 }
-var gen = logGenerator();
-// the first call of next executes from the start of the function
-// until the first yield statement
-gen.next();
-gen.next('pretzel'); // pretzel
-gen.next('california'); // california
-gen.next('mayonnaise'); // mayonnaise
+var iterator = generator();
+// Start execution till we get first yield value
+var foo = iterator.next();
+console.log(foo.value);
+// como está comentado la excepción no se ejuta ya que no hay un 'next()'
+//var foo = iterator.next();
