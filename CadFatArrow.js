a0643e1b7eb73f8db13e2e6b0b8578f5bf63b1aa
unknown <camachoedgar23@gmail.com>
2019-08-31 19:32:26 -0600

    DesestructuracionDeObjetos

--- CadFatArrow.js
+++
@@ -1,8 +0,0 @@
-//	A	curried	function 
-var add = function (x) { return function (y) { return x + y; }; };
-//	Simple	usage 
-add(123)(456);
-//	partially	applied 
-var add123 = add(123);
-//	fully	apply	the	function 
-add123(456);
