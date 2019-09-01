b3bda2e1aa6df47cdda104a1ed40f8b08a2fb1f5
unknown <camachoedgar23@gmail.com>
2019-08-31 19:40:15 -0600

    Estructuración

--- Desestruc.js
+++
@@ -1,3 +0,0 @@
-var array = [1, 2, 3, 4];
-var x = array[0], y = array[1], rest = array.slice(2);
-console.log(x, y, rest); //	1,	2,	[3,4]
--- Desestruc.ts
+++
@@ -1,3 +0,0 @@
-var	array = [1, 2, 3, 4];
-var [x,	y, ...rest] = array;
-console.log(x, y, rest); //	1,	2,	[3,4]
