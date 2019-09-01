27fd72b4ae42d89e6281a8ba333ea18ce4cb6d21
unknown <camachoedgar23@gmail.com>
2019-08-31 19:36:31 -0600

    DesestructuracionDeArraysConEstructuración

--- Desestruc.ts
+++ Desestruc.ts
@@ -1,4 +1,3 @@
-var	array = [1, 2, 3];
-console.log(array[0]);	//	1
-var	[x, y, z] = array;
-console.log(x);	//	1
+var	array = [1, 2, 3, 4];
+var [x,	y, ...rest] = array;
+console.log(x, y, rest); //	1,	2,	[3,4]
