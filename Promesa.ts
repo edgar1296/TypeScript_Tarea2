6487d1324cc4a73110a5bb64d4f9d96a98db2cb6
unknown <camachoedgar23@gmail.com>
2019-08-31 21:08:52 -0600

    CadenaDePromesas

--- Promesa.ts
+++ Promesa.ts
@@ -1,9 +1,16 @@
-const promise = new Promise((resolve, reject) => {
-    reject(new Error("Algo malo a pasado"));
-});
-promise.then((res) => {
-    // This is never called 
-});
-promise.catch((err) => {
-    console.log('Tengo una llamada: ', err.message); // Tengo una llamada: 'Algo malo a pasado'
-});
+Promise.reject(new Error('something	bad	happened'))
+    .then((res) => {
+        console.log(res);	//	not	called
+        return	456;
+    })
+    .then((res)	=> {
+        console.log(res);	//	not	called
+        return	Promise.resolve(123);
+    })
+    .then((res) => {
+        console.log(res);	//	not	called
+        return	Promise.resolve(123);
+    })
+    .catch((err) => { 
+        console.log(err.message);	//	something	bad	happened
+    });
