79afb0c99c1ce67d8b9b10dfad362e9d8f14ec3d
unknown <camachoedgar23@gmail.com>
2019-09-01 14:05:56 -0600

    Generator3

--- Generator.ts
+++ Generator.ts
@@ -1,12 +1,12 @@
-function* idMaker() {
-    let index = 0;
-    while (index < 3)
-        yield index++;
+function* generator(){
+    console.log('Execution started');
+    yield 0;
+    console.log('Execution resumed');
+    yield 1;
+    console.log('Execution resumed');
 }
-let gen = idMaker();
-console.log(gen.next()); // { value: 0, done: false }
-console.log(gen.next()); // { value: 1, done: false }
-console.log(gen.next()); // { value: 2, done: false }
-console.log(gen.next()); // { value: undefined, done: true }
-// Accede a la variable por que se le ha dicho apesar de que el 'yield' no ha sido efectuado.
-    
+var iterator = generator();
+console.log('Starting iteration'); // Esto se ejecutará antes que nada de dentro del método generator()
+console.log(iterator.next()); // { value: 0, done: false }
+console.log(iterator.next()); // { value: 1, done: false }
+console.log(iterator.next()); // { value: undefined, done: true }
